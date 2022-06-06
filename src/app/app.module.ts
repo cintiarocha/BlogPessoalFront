9e591d988ee1494ba62d8a8c2cef645b97633d3d
Cintia Rocha <cintia.rocha95@gmail.com>
2022-06-06 11:41:28 -0300

    Implementação de Componente Menu e Rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,

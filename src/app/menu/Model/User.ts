f0c70c6073c487ecc5c452d64b6b7168256ec976
Cintia Rocha <cintia.rocha95@gmail.com>
2022-06-07 13:25:33 -0300

    Rotas de entrar e cadastrar

--- src/app/menu/Model/User.ts
+++ src/app/menu/Model/User.ts
@@ -1,11 +1,13 @@
+import { Postagem } from "./Postagem";
+
 export class User {
    public id: number;
    public nome: string;
    public usuario: string;
    public senha: string;
    public foto: string;
-   public Type: string;
-
+   public Tipo: string;
+   public postagem: Postagem []
 
 
 		
